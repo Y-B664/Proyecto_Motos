418517dc8bdbbea9380efc2cdadbd7d9f17bae52
BenavidesY <231582951+Y-B664@users.noreply.github.com>
2026-09-21 07:14:38 -0500

    Se modificaron los metodos de la clase GestorParqueadero, se modifico la dependencia de JUnit

--- src/test/GestorParqueaderoTest.java
+++ src/test/GestorParqueaderoTest.java
@@ -2,8 +2,8 @@ import co.edu.unilibre.datos.Moto;
 import co.edu.unilibre.datos.Parqueadero;
 
 import co.edu.unilibre.datos.RegistroPago;
-import co.edu.unilibre.datos.TipoPago;
 import co.edu.unilibre.gestion.GestorParqueadero;
+import co.edu.unilibre.gestion.placa;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -35,12 +35,12 @@ public class GestorParqueaderoTest {
     }
     @Test
     public void darSalidaMotoOk(){
-        assertTrue(gestorParqueadero.darSalidaMoto("HHH-980", TipoPago.DAVIPLATA));
+        assertTrue(gestorParqueadero.darSalidaMoto("HHH-980"));
     }
     @Test
     public void generarRegistroPagoOk(){
         RegistroPago registroPago = new RegistroPago();
-        assertEquals(registroPago,gestorParqueadero.generarRegistroPago(new Moto()));
+        assertEquals(registroPago,gestorParqueadero.generarRegistroPago(new placa(new Moto())));
     }
     @Test
     public void calcularValorAPagarOk(){
